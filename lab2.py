3144929ef69c7b27c2e4f8bd4a69588533d71c3f
Andrew Lai <arjlai221@gmail.com>
2021-10-15 14:32:16 -0700

    lab2 progress

--- lab2.py
+++ lab2.py
@@ -4,74 +4,111 @@ import pandas as pd
 class lab2():
     def __init__(self):
         self.athleteEvents = pd.read_csv("athlete_events.csv")
-        self.nocRegions = pd.read_csv("noc_regions.csv")
-        self.nocRegionsDict = dict(self.nocRegions[["NOC", "region"]].values)
+        # self.nocRegions = pd.read_csv("noc_regions.csv")
+        # self.nocRegions = dict(self.nocRegions[["NOC", "region"]].values)
 
-        self.q2()
+        # self.tester()
+        # self.q2()
         # self.q3()
         # self.q4()
-        # self.q5()
+        self.q5()
+
+    def tester(self):
+        # athleteEventsTest = pd.read_csv("athlete_events_test.csv").fillna(0)
+        # athleteEventsTest = athleteEventsTest[athleteEventsTest["Medal"] != 0][["NOC", "Medal"]]
+        # nocRegionsTest = pd.read_csv("noc_regions_test.csv")[["NOC", "region"]]
+        # merged = pd.merge(nocRegionsTest, athleteEventsTest)
+        # print(merged)
+
+        # athleteEventsTest = pd.read_csv("athlete_events_test.csv").fillna(0)
+        # athleteEventsTest = athleteEventsTest[athleteEventsTest["Medal"] != 0][["ID", "Name", "Medal"]]
+        # medalCountsPerAthlete = athleteEventsTest["ID"].value_counts().to_frame()
+        # medalCountsPerAthlete["MedalCount"] = medalCountsPerAthlete["ID"]
+        # medalCountsPerAthlete["ID"] = medalCountsPerAthlete.index
+        # print(medalCountsPerAthlete)
+        # athleteEventsTest = athleteEventsTest.set_index("ID").drop_duplicates().reset_index()
+        # print(athleteEventsTest)
+        # merged = pd.merge(athleteEventsTest, medalCountsPerAthlete)
+        # print(merged)
+
+        # athleteEventsTest = pd.read_csv("athlete_events_test.csv").fillna(0)[["NOC", "Year", "Medal"]]
+        # athleteEventsTest = athleteEventsTest[athleteEventsTest["Medal"] != 0]
+        # athleteEventsTest = athleteEventsTest[athleteEventsTest["Year"] >= 1960]
+        # nocGroup = athleteEventsTest.groupby("NOC")
+        # yearCounts = nocGroup['Year'].value_counts().unstack()
+        # print(yearCounts)
+        # popSizeTest = pd.read_csv("population_size_test.csv")[["Country Code", "1992", "2012"]]
+        # print(popSizeTest)
+
+        pass
+
+    # def q2(self):
+    #     golds = self.athleteEvents[self.athleteEvents["Medal"] == "Gold"][["NOC", "Medal"]]
+    #     golds["NOC"] = golds["NOC"].map(lambda noc: self.nocRegions[noc])
+    #     golds = golds["NOC"].value_counts()
+    #     print(golds)
 
     def q2(self):
         golds = self.athleteEvents[self.athleteEvents["Medal"] == "Gold"][["NOC", "Medal"]]
-        # golds["NOC"] = golds["NOC"].map(lambda noc: self.nocRegions[noc])
-        golds = golds["NOC"].value_counts()
-        print(golds)
+        nocRegions = pd.read_csv("noc_regions.csv")[["NOC", "region"]]
+        merged = pd.merge(golds, nocRegions)
+        print(merged["region"].value_counts())
+
 
     def q3(self):
-        golds = self.athleteEvents[self.athleteEvents["Medal"] == "Gold"][["ID", "Name", "Medal"]]
-        idCounts = golds["ID"].value_counts(sort = False)
-        golds = golds.drop_duplicates()
-        golds["Medal"] = golds["ID"].map(lambda id : idCounts.loc[id])
-        print(golds.sort_values(by = "Medal", ascending = False))
+        golds = self.athleteEvents[self.athleteEvents["Medal"] == "Gold"][["ID", "Name"]]
+        medalCountsPerAthlete = golds["ID"].value_counts().to_frame()
+        medalCountsPerAthlete["MedalCount"] = medalCountsPerAthlete["ID"]
+        medalCountsPerAthlete["ID"] = medalCountsPerAthlete.index
+        golds = golds.set_index("ID").drop_duplicates().reset_index()
+        merged = pd.merge(golds, medalCountsPerAthlete)
+        print(merged.sort_values(by = "MedalCount", ascending = False))
+
 
     def q4(self):
-
-        # fill all na's with 0's
         medals = self.athleteEvents.fillna(0)
-
-        # remove all entries that didnt result in a medal won
         medals = medals[medals["Medal"] != 0]
-        # print(medals[["ID", "Height", "Medal"]])
+        medals = medals[medals["Height"] > 0][["ID", "Name", "Height"]]
+        medalCountsPerAthlete = medals["ID"].value_counts().to_frame()
+        medalCountsPerAthlete["MedalCount"] = medalCountsPerAthlete["ID"]
+        medalCountsPerAthlete["ID"] = medalCountsPerAthlete.index
+        medals = medals.set_index("ID").drop_duplicates().reset_index()
+        merged = pd.merge(medals, medalCountsPerAthlete)
 
-        # remove all entries that have 0 height
-        medals = medals[medals["Height"] > 0][["ID", "Height"]]
-        # print(medals)
+        # print(merged)
+        print(merged["MedalCount"].corr(merged["Height"]))
 
-        # count the number of medals each athlete has won
-        medalCount = medals["ID"].value_counts()
-
-        # change dataframe to have only one entry per athlete
-        medals = medals.set_index("ID").drop_duplicates()
-
-        # map the medal count to each athlete ID
-        medals["MedalCount"] = medals.index.map(lambda id : medalCount.loc[id])
-        
-        # print(medals)
-        print(medals["MedalCount"].corr(medals["Height"]))
 
     def q5(self):
+        athleteEvents = pd.read_csv("athlete_events.csv").fillna(0)[["NOC", "Year", "Medal"]]
+        athleteEvents = athleteEvents[athleteEvents["Medal"] != 0]
+        athleteEvents = athleteEvents[athleteEvents["Year"] >= 1960]
+        print(athleteEvents.head(20))
+        nocGroup = athleteEvents.groupby("NOC")
+        yearCounts = nocGroup['Year'].value_counts().fillna(0)
+        yearCounts.columns = ['MedalCount']
+        yearCounts.name = "MedalCount"
+        yearCounts = yearCounts.reset_index()
 
 
-        # import population_size.csv
-        yearlyPopulation = pd.read_csv("population_size.csv").drop(columns=["Country Name","Indicator Name","Indicator Code"]).fillna(0)
-        yearlyPopulation.set_index("Country Code", inplace = True)
-        print(yearlyPopulation)
+        # print(yearCounts)
+        popSize = pd.read_csv("population_size.csv").drop(columns=["Country Name","Indicator Name","Indicator Code"])
+        popSize = popSize.rename(columns = {"Country Code": "NOC"}).set_index("NOC").stack()
+        popSize.name = "popSize"
+        popSize = popSize.reset_index()
+        # print(popSize)
+
+        print(yearCounts.head(30))
+        # print('\n')
+        print(popSize.head(30))
+
+        #print(yearCounts[["NOC", 1960]].set_index("NOC")[1960])
+        #print(popSize[["NOC", "1960"]].set_index("NOC")["1960"])
 
 
-        # strip out all non-medal entries, isolate year and NOC columns
-        medalsPerYearPerCountry = self.athleteEvents.fillna(0)[self.athleteEvents["Medal"] != 0][["NOC", "Year"]]
+        
 
-        # count the number of medals won by each country each year
-            # create dataframe with indexes and column labels same as yearly population
-            # fill all data with 0's
-            # for each element in medalsPerYearPerCountry
-            # increment [NOC, Year] by 1
-
-        # get list of all NOC's
-
-        # for each column of yearly population and medals won per year, get correlation
-
+        
 
     def getYearlyCalculation(self, country, yearlyPop, medalsPerCountry):
         pass
