38ce1b4ca7c8b9c6e55d9093a38dc4d07a6c19ed
Andrew Lai <arjlai221@gmail.com>
2021-11-09 19:37:43 -0800

    fixed assignment 4

---
+++ labs/lab6/lab6.py
@@ -0,0 +1,16 @@
+
+
+def computeDistance(query, docTitle):
+    pass
+
+def importDocuments(folder):
+    pass
+
+def generateTFIDF(documents):
+    pass
+
+def importHumanJudgement(filepath):
+    pass
+
+def calculateMeanAveragePrecision():
+    pass
